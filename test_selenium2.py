c19597a26c3d620d852865a513402aec881eae64
mitragroups <miftahmuhajir@gmail.com>
2021-03-16 15:35:39 +0700

    test selenium

--- test_selenium2.py
+++ test_selenium2.py
@@ -15,6 +15,6 @@ while len(stack) > 0 and len(result) < 5:
     elements = driver.find_elements_by_tag_name("a")
     for elem in elements:
         url = elem.get_attribute('href')
-        if url not in stack and url not in result and '#' not in url and 'https://www.techlistic.com/p/demo-selenium-practice.html' in url:
+        if url not in stack and url not in result or '#' not in url and 'https://www.techlistic.com/p/demo-selenium-practice.html' in url:
             stack.append(url)
 print(result)
